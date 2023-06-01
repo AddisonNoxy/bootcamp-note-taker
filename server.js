67722d89436bb5f6c6195b49b7deb7b950d9c64d
AddisonNoxy <drolknil@gmail.com>
2023-06-01 15:25:59 -0400

    added uuid, added middleware for parsing information

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 var fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 const notesFile = require("./db/db.json");
 
@@ -10,22 +11,29 @@ const PORT = 3001;
 
 app.use(express.static("public"));
 
+app.use(express.json()); //parses application/json
+app.use(express.urlencoded({extended: true}));
+
+//create GET route for main directory
 app.get('/', (req, res) => {
     console.log("Root directory found!");
     res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
+//create GET route for the notes file
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 })
 
+//GET route to return the notes which have been posted in db.json
 app.get('/api/notes', (req, res) => {
     res.json(notesFile);
 })
 
 app.post('/api/notes', (req, res) => {
     console.log(`${req.method} request received`);
-    
+    console.log(req.body);
+
 })
 
 app.listen(PORT, () => {
